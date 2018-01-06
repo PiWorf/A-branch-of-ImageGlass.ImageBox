c3f03882c5f4ff6e0cce10bac83a7a4f89f59710
PiWolf <893265845@qq.com>
2018-01-06 15:13:15 +0800

    提交文件

---
+++ Sample/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Sample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ImageGlass.ImageBox.DefaultGifAnimator defaultGifAnimator2 = new ImageGlass.ImageBox.DefaultGifAnimator();
+            this.imageBox1 = new ImageGlass.ImageBox.ImageBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.rToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Animator = defaultGifAnimator2;
+            this.imageBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox1.Location = new System.Drawing.Point(0, 28);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(569, 389);
+            this.imageBox1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(569, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // rToolStripMenuItem
+            // 
+            this.rToolStripMenuItem.Name = "rToolStripMenuItem";
+            this.rToolStripMenuItem.Size = new System.Drawing.Size(89, 24);
+            this.rToolStripMenuItem.Text = "Recovery";
+            this.rToolStripMenuItem.Click += new System.EventHandler(this.rToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(56, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 417);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Sample";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ImageGlass.ImageBox.ImageBox imageBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem rToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+    }
+}
+
---
+++ Sample/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sample
+{
+    public partial class Form1 : Form
+    {
+        string myPath;
+        string picPath;
+
+        public Form1()
+        {
+            InitializeComponent();
+            myPath = System.Environment.CurrentDirectory;
+            picPath = myPath + "\\pic.jpg";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetPicture(picPath);
+        }
+
+        /// <summary>
+        /// Give the picture to ImageBox
+        /// </summary>
+        /// <param name="picPath">file path of the picture</param>
+        private void GetPicture(string picPath)
+        {
+            //How to use ImageClass.ImageBox
+            //that is quite easy
+            //All you have to do is giving the file path of the picture
+            imageBox1.Image = Image.FromFile(picPath);
+        }
+
+        private void rToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            GetPicture(picPath);
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string help = "How to use ?"+"\r\n" + "\r\n";
+            help += "This ImageBox control allows you to browse, zoom and move pictures." + "\r\n" + "\r\n";
+            help += "You can move the right mouse button to make a screenshot.";
+            MessageBox.Show(help,"help");
+        }
+    }
+}
